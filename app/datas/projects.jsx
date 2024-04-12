0dc44716641a100cbc5b0dcfea17522c1b70a15f
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-13 01:43:17 +0200

    add fake datas for projects

---
+++ app/datas/projects.jsx
@@ -0,0 +1,75 @@
+export const webAppProjects = [
+    {
+        id: 1,
+        title: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project1.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 2,
+        title: 'Project 2',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project2.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 3,
+        title: 'Project 3',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project3.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 4,
+        title: 'Project 4',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project4.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 5,
+        title: 'Project 5',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project5.jpg',
+        link: 'https://www.google.com'
+    }
+]
+
+export const softwareProjects = [
+    {
+        id: 1,
+        title: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project1.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 2,
+        title: 'Project 2',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project2.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 3,
+        title: 'Project 3',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project3.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 4,
+        title: 'Project 4',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project4.jpg',
+        link: 'https://www.google.com'
+    },
+    {
+        id: 5,
+        title: 'Project 5',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quam excepturi corrupti ratione laboriosam deleniti rerum laborum.',
+        image: '/project5.jpg',
+        link: 'https://www.google.com'
+    }
+]
