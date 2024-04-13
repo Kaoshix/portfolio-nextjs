a2e94923620ed839e7633acd0336f748c36ef260
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-13 16:53:43 +0200

    add feature to download cv

--- app/components/ui/button.jsx
+++ app/components/ui/button.jsx
@@ -1,5 +1,5 @@
 export const Button = ({ children }) => {
     return (
-        <button type="button" className="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-4 py-2 dark:text-grey-600 dark:border-gray-600 dark:hover:bg-gray-200 dark:hover:border-gray-600 dark:focus:ring-gray-700">{children}</button>
+        <a type="button" href="/cv-olhagaray-sebastien-dev.pdf" download className="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-4 py-2 dark:text-grey-600 dark:border-gray-600 dark:hover:bg-gray-200 dark:hover:border-gray-600 dark:focus:ring-gray-700">{children}</a>
     )
 }
