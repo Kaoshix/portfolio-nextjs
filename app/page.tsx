5182e789d649bd37159acea21c0c1339a9ce92a4
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-13 20:25:59 +0200

    modif marges

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
 
     <main className="max-w-screen-2xl m-auto overflow-hidden flex flex-col gap-5">
 
-      <div className="min-h-screen mx-5 flex flex-col mb-10 md:mb-0" id="home-section">
+      <div className="min-h-screen mx-5 flex flex-col" id="home-section">
         <Header />
-        <div className="flex flex-col grow mb-20 md:mb-0 mt-5">
+        <div className="flex flex-col grow">
           <HeroBanner />
           <div className="min-h-[50px] md:min-h-0">
             <ArrowHomeButton />
