08e2d93d4096c0e72e6cdf70d85c3bc421f1fdd9
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-13 20:22:22 +0200

    update some spaces between divs

--- app/components/ui/arrow-home-button.jsx
+++ app/components/ui/arrow-home-button.jsx
@@ -1,7 +1,7 @@
 export const ArrowHomeButton = () => {
     return (
         <a href="#project-section">
-            <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 rotate-90">
+            <div className="absolute bottom-5 md:bottom-8 left-1/2 transform -translate-x-1/2 rotate-90">
                 <div className="animate-[propel_5s_infinite_both] opacity-80">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="h-10 w-10">
                         <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
 
   return (
 
-    <main className="max-w-screen-2xl m-auto overflow-hidden">
+    <main className="max-w-screen-2xl m-auto overflow-hidden flex flex-col gap-5">
 
       <div className="min-h-screen mx-5 flex flex-col mb-10 md:mb-0" id="home-section">
         <Header />
-        <div className="flex flex-col grow mb-20 mt-5">
+        <div className="flex flex-col grow mb-20 md:mb-0 mt-5">
           <HeroBanner />
-          <div className="min-h-[50px]">
+          <div className="min-h-[50px] md:min-h-0">
             <ArrowHomeButton />
           </div>
         </div>
