59756ba0da1bb9848110ebc05447f9985c738d34
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-14 15:27:34 +0200

    first try contact form with real email

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import { HeroBanner } from "./components/hero-banner";
 import { Header } from "./components/header";
 import { ProjectsContainer } from "./components/projects-container";
 import { ArrowHomeButton } from "./components/ui/arrow-home-button";
+import { Form } from "./components/form";
 
 export default function Home() {
-
   return (
 
     <main className="max-w-screen-2xl m-auto overflow-hidden flex flex-col gap-5">
+      <Form />
 
       <div className="min-h-screen mx-5 flex flex-col pb-20" id="home">
         <Header />
