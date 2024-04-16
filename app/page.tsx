87a7ed2e5af827def448585eb4e4fc89f4a662c1
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-04-16 18:14:52 +0200

    clean up code and add new website name and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { HeroBanner } from "./components/hero-banner";
-import { Header } from "./components/header";
 import { ProjectsContainer } from "./components/projects-container";
 import { ArrowHomeButton } from "./components/ui/arrow-home-button";
 import { ContactContainer } from "./components/contact-container";
@@ -9,23 +8,24 @@ export default function Home() {
 
     <main className="max-w-screen-2xl m-auto overflow-hidden flex flex-col gap-5">
 
+      {/* Home section */}
       <div className="min-h-screen mx-5 flex flex-col pb-20" id="home">
-        <Header />
         <div className="flex flex-col justify-center gap-14 md:gap-20 grow">
           <HeroBanner />
-          <div>
-            <ArrowHomeButton />
-          </div>
+          <ArrowHomeButton />
         </div>
       </div>
 
+      {/* Projects section */}
       <div className="min-h-screen mb-20" id="projects">
         <ProjectsContainer />
       </div>
 
+      {/* Contact section */}
       <div className="min-h-screen relative" id="contact">
         <ContactContainer />
       </div>
+
     </main>
 
 
