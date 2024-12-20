331341675381dbc2f279b456b27e00a3d8e4ce47
OLHAGARAY Sebastien <sebastien@levenly.com>
2024-12-20 21:38:22 +0100

    update website links aria label for better accessibility

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,15 +9,15 @@ export const Footer = () => {
             <div className="max-w-screen-2xl m-auto flex flex-col gap-5 justify-center items-center h-full">
                 <p className="text-xl">© 2024 Sebastien Olhagaray</p>
                 <div className="flex gap-5">
-                    <Link href="https://www.linkedin.com/in/sebastien-olhagaray" target='_blank'>
+                    <Link href="https://www.linkedin.com/in/sebastien-olhagaray" target='_blank' aria-label="Profil Linkedin de Sébastien OLHAGARAY">
                         <FaLinkedin size={40} />
                     </Link>
 
-                    <Link href="https://github.com/Kaoshix" target='_blank'>
+                    <Link href="https://github.com/Kaoshix" target='_blank' aria-label="Profil GitHub de Sébastien OLHAGARAY">
                         <IoLogoGithub size={40} />
                     </Link>
 
-                    <Link href="https://www.instagram.com/kaoshi_x" target='_blank'>
+                    <Link href="https://www.instagram.com/kaoshi_x" target='_blank' aria-label="Profil Instagram de Sébastien OLHAGARAY">
                         <IoLogoInstagram size={40} />
                     </Link>
                 </div>
--- app/components/ui/arrow-home-button.jsx
+++ app/components/ui/arrow-home-button.jsx
@@ -2,7 +2,7 @@ import { FaArrowDown } from "react-icons/fa6";
 
 export const ArrowHomeButton = () => {
     return (
-        <a href="#projects" className="block w-fit animate-[bouncing_3s_infinite] mx-auto mb-10">
+        <a href="#projects" className="block w-fit animate-[bouncing_3s_infinite] mx-auto mb-10" aria-label="Projects list">
             <FaArrowDown size={40} />
         </a>
     )
