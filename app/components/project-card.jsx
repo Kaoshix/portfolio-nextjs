b2b940941f923914e71c88cf72f0e40f294d941a
OLHAGARAY Sebastien <sebastien@levenly.com>
2024-12-20 22:32:34 +0100

    fix bug project card link

--- app/components/project-card.jsx
+++ app/components/project-card.jsx
@@ -4,12 +4,12 @@ export const ProjectCard = ({ project }) => {
     return (
 
         <>
-            {project.platform === 'pro' ? (
+            {project.link ? (
                 <a href={project.link} target="_blank" className="flex flex-col rounded overflow-hidden shadow-lg bg-white border border-gray-200 md:hover:scale-105 md:duration-200">
                     <ProjectContent project={project} />
                 </a>
             ) : (
-                <div className="flex flex-col rounded overflow-hidden shadow-lg bg-white border border-gray-200">
+                <div className="flex flex-col rounded overflow-hidden shadow-lg bg-white border border-gray-200 opacity-50">
                     <ProjectContent project={project} />
                 </div>
 
